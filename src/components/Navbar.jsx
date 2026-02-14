144018e543521e80387b97855d57b1bae48622fb
Ronee <137155067+RONHEL235@users.noreply.github.com>
2026-02-14 20:08:51 +0200

    Its all about books now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ export default function Navbar() {
 
   const links = [
     { name: "Home", to: "/" },
-    { name: "Projects", to: "/projects" },
-    { name: "Writings", to: "/writings" },
+    { name: "Software", to: "/projects" },
+    { name: "Words", to: "/writings" },
     { name: "Graphics", to: "/graphics" },
   ];
 
